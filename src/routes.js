5dba0dfdb3710dce5ca32eeaababb0ae5f5ec33c
Phumthawan Nokun <knzphumthawan@gmail.com>
2023-04-24 23:35:57 +0700

    update dependencies

--- src/routes.js
+++ src/routes.js
@@ -21,7 +21,6 @@ import TableList from "views/TableList.js";
 import Typography from "views/Typography.js";
 import Icons from "views/Icons.js";
 import Maps from "views/Maps.js";
-import Notifications from "views/Notifications.js";
 import Upgrade from "views/Upgrade.js";
 
 const dashboardRoutes = [
