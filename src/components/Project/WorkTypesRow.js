73a235e47ad4a8d32baa61c88153eebae45c214b
Phumthawan Nokun <knzphumthawan@gmail.com>
2023-04-29 17:05:14 +0700

    add worktype table

---
+++ src/components/Project/WorkTypesRow.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Button, Form } from "react-bootstrap";
+
+function TableRows({ rowsData, deleteTableRows, handleChange }) {
+  return rowsData.map((data, index) => {
+    const { name, id } = data;
+    return (
+      <tr key={index}>
+        <td>
+          <Form.Control
+            size="sm"
+            type="text"
+            placeholder="name"
+            defaultValue={name}
+          />
+        </td>
+        <td align="center">
+          <Button
+            onClick={() => deleteTableRows(index, id)}
+            className="btn-fill ms-1"
+            variant="danger"
+            size="sm"
+          >
+            Delete
+          </Button>{" "}
+        </td>
+      </tr>
+    );
+  });
+}
+export default TableRows;
