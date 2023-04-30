1f476df4b8a21277bdd582a0d65e75bfa2b3ec8e
Phumthawan Nokun <knzphumthawan@gmail.com>
2023-04-30 21:20:44 +0700

    remove unuse route

--- src/routes.js
+++ src/routes.js
@@ -16,12 +16,6 @@
 
 */
 import Dashboard from "views/Dashboard.js";
-import UserProfile from "views/UserProfile.js";
-import TableList from "views/TableList.js";
-import Typography from "views/Typography.js";
-import Icons from "views/Icons.js";
-import Maps from "views/Maps.js";
-import Upgrade from "views/Upgrade.js";
 import Status from "views/Status"
 import Role from "views/Role";
 import User from "views/User";
@@ -35,53 +29,10 @@ import ReportTotalUser from "views/ReportTotalUser";
 
 const dashboardRoutes = [
   {
-    upgrade: true,
-    path: "/upgrade",
-    name: "Upgrade to PRO",
-    icon: "nc-icon nc-alien-33",
-    component: Upgrade,
-    layout: "/admin",
-  },
-  {
     path: "/dashboard",
     name: "Dashboard",
     icon: "nc-icon nc-chart-pie-35",
     component: Dashboard,
-    layout: "/admin"
-  },
-  {
-    path: "/user/profile",
-    name: "User Profile",
-    icon: "nc-icon nc-circle-09",
-    component: UserProfile,
-    layout: "/admin"
-  },
-  {
-    path: "/table",
-    name: "Table List",
-    icon: "nc-icon nc-notes",
-    component: TableList,
-    layout: "/admin"
-  },
-  {
-    path: "/typography",
-    name: "Typography",
-    icon: "nc-icon nc-paper-2",
-    component: Typography,
-    layout: "/admin"
-  },
-  {
-    path: "/icons",
-    name: "Icons",
-    icon: "nc-icon nc-atom",
-    component: Icons,
-    layout: "/admin"
-  },
-  {
-    path: "/maps",
-    name: "Maps",
-    icon: "nc-icon nc-pin-3",
-    component: Maps,
     layout: "/admin"
   },
   {
@@ -101,35 +52,35 @@ const dashboardRoutes = [
   {
     path: "/reportTotalTime",
     name: "Report Total Time",
-    icon: "nc-icon nc-time-alarm",
+    icon: "nc-icon  nc-single-copy-04",
     component: ReportTotalTime,
     layout: "/admin"
   },
   {
     path: "/reportIndividualTimeEntry",
     name: "Report Individual Time Entry",
-    icon: "nc-icon nc-time-alarm",
+    icon: "nc-icon  nc-single-copy-04",
     component: ReportIndividualTimeEntry,
     layout: "/admin"
   },
   {
     path: "/reportMemberInProject",
     name: "Report Member In Project",
-    icon: "nc-icon nc-time-alarm",
+    icon: "nc-icon  nc-single-copy-04",
     component: ReportMemberInProject,
     layout: "/admin"
   },
   {
     path: "/reportTotalProject",
     name: "Report Total Project",
-    icon: "nc-icon nc-time-alarm",
+    icon: "nc-icon  nc-single-copy-04",
     component: ReportTotalProject,
     layout: "/admin"
   },
   {
     path: "/reportTotalUser",
     name: "Report Total User",
-    icon: "nc-icon nc-time-alarm",
+    icon: "nc-icon  nc-single-copy-04",
     component: ReportTotalUser,
     layout: "/admin"
   },
